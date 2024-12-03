45d01c4457f919b41500d29805924245ff1b83cf
David Simon <d@vidsimon.tech>
2024-12-03 09:58:34 -0500

    Solve day1

---
+++ day1/src/main.rs
@@ -0,0 +1,59 @@
+use anyhow::Result;
+use std::{
+    collections::HashMap,
+    io::{stdin, BufRead},
+};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Failed to parse input")]
+    ParseError,
+}
+
+fn parse_lists(inp: impl BufRead) -> Result<(Vec<u64>, Vec<u64>)> {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+    for line in inp.lines() {
+        let s = line?;
+        let (ls, rs) = s.split_once("   ").ok_or(Error::ParseError)?;
+
+        left.push(ls.parse()?);
+        right.push(rs.parse()?);
+    }
+
+    Ok((left, right))
+}
+
+fn total_distance(mut l: Vec<u64>, mut r: Vec<u64>) -> u64 {
+    l.sort();
+    r.sort();
+
+    l.iter().zip(r.iter()).map(|(&l, &r)| l.abs_diff(r)).sum()
+}
+
+fn freq_map(v: &Vec<u64>) -> HashMap<u64, u64> {
+    let mut res = HashMap::new();
+    for &elem in v {
+        *res.entry(elem).or_default() += 1;
+    }
+
+    res
+}
+
+fn similarity_score(l: &Vec<u64>, r: &Vec<u64>) -> u64 {
+    let right_freqs = freq_map(r);
+
+    l.iter()
+        .map(|&v| v * right_freqs.get(&v).copied().unwrap_or(0))
+        .sum()
+}
+
+fn main() -> Result<()> {
+    let (l, r) = parse_lists(stdin().lock())?;
+
+    println!("{}", total_distance(l.clone(), r.clone()));
+    println!("{}", similarity_score(&l, &r));
+
+    Ok(())
+}
